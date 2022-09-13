439300d212ff8d875a6765b5575018af29d9bf58
Brenda <biibii-ou>
2022-09-12 18:35:18 -0700

    Variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Start the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z', i++)
+{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
