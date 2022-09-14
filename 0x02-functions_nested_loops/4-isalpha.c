a387956e00284e24f8a170c84077d2bcc0ea9c47
Brenda <biibii-ou>
2022-09-14 15:49:25 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
