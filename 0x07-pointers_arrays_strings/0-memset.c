54df3b65ec0faa405e590fd9aa15dd99e0afb29d
Brenda <biibii-ou>
2022-09-27 04:31:43 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * _memset - sets a given amount of bytes to a value at an address
- *
- * @s: memory location to start at
- * @b: character to write to memory
- * @n: number of bytes to write
- *
- * Return: pointer to s
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
 
-	while (n-- > 0)
-		*ptr++ = b;
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+
+	} /*END FOR*/
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * _memcpy - copies n bytes from source memory to destination
- *
- * @dest: destination to write to
- * @src: source to copy from
- * @n: number of bytes to write
- *
- * Return: pointer to dest
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
  */
-char *_memcpy(char *dest, char *src, int n)
-{
-	char *ptr = dest;
 
-	while (n--)
-		*ptr++ = *src++;
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+
+	} /*END WHILE*/
+
 
 	return (dest);
 }
